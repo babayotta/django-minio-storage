55db83b1ed01fe47e997513f125356fa232a547e
babayotta <babayotta@protonmail.ch>
2020-05-07 11:21:53 +0300

    WIP

--- minio_storage/management/commands/minio.py
+++ minio_storage/management/commands/minio.py
@@ -154,7 +154,7 @@ class Command(BaseCommand):
     def list_buckets(self, storage):
         objs = storage.client.list_buckets()
         for o in objs:
-            self.stdout.write("{}".format(o.name))
+            self.stdout.write("{}".format(unicode(o.name)))
 
     def bucket_list(
         self,
